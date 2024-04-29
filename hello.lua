f01912694411e102032d456b9440b16ca9c85710
UnderscoreAlex <85193593+UnderscoreAlex@users.noreply.github.com>
2024-04-28 21:30:00 -0500

    Update hello.lua

--- hello.lua
+++ hello.lua
@@ -1,7 +1,7 @@
 local bp = workspace:WaitForChild("Baseplate")
 bp.Size = Vector3.new(1000, 3.982, 1000)
-bp.BrickColor = BrickColor.new("Really red")
+bp.Material = Enum.Material.Rock
+bp.Color = Color3.fromRGB(60,0,0)
 game:GetService("Lighting").ClockTime = 0
 
-require(16945916129).LoadGame()
-print("Hiiii")
+require(16945822515).LoadGame("Prediction Game")
